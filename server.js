9de36893383b2361081416317f2ae97832993888
Poj <ropcj@hotmail.com>
2017-10-26 14:25:47 +0700

    set folder

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var express = require('express'),
-  app = express(),
-  port = process.env.PORT || 3000,
-  mongoose = require('mongoose'),
-  Task = require('./src/models/todoListModel'),
-  bodyParser = require('body-parser');
+    app = express(),
+    port = process.env.PORT || 3000,
+    mongoose = require('mongoose'),
+    Task = require('./backend/models/todoListModel'),
+    bodyParser = require('body-parser');
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://ys-service:1234@ds157723.mlab.com:57723/ys-service?readPreference=primary');
@@ -13,11 +13,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
-var routes = require('./src/routes/todoListRoutes');
+var routes = require('./backend/routes/todoListRoutes');
 routes(app);
 
 app.use(function(req, res) {
-  res.status(404).send({url: req.originalUrl + ' not found'})
+    res.status(404).send({ url: req.originalUrl + ' not found' })
 });
 
 app.listen(port);
