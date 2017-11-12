50bb82b39a793e6286b40ec96db9f9bffbafa951
Teerapord Lin <teerapord.dmit.co.th>
2017-11-12 22:46:47 +0700

    first commit

--- frontend/routes.js
+++ frontend/routes.js
@@ -5,4 +5,8 @@ module.exports = function(app) {
 
     app.use(express.static(__dirname + '/'));
 
+    app.post('/test', function(request, response){
+        console.log(request.body);
+        //console.log(request.body.description,'des');
+    });
 };
