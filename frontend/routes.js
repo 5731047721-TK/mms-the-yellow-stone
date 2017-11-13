6d547e580b594c94009fee72e663563fc8aaa0e5
Norawit Nangsue <norawit.n@student.chula.ac.th>
2017-11-13 22:19:05 +0700

    Be able to register

--- frontend/routes.js
+++ frontend/routes.js
@@ -1,8 +1,24 @@
 'use strict';
-var express = require('express')
+var express = require('express');
+var request = require('request');
 
 module.exports = function(app) {
 
     app.use(express.static(__dirname + '/'));
 
+    app.route('/register/submit')
+        .post((req,res)=>{
+            console.log("poj see" , req.body);
+            request.post(
+                {url:'https://ys2.herokuapp.com/api/user/',
+                form: req.body},
+                function (error, response, body) {
+                    if (!error && response.statusCode == 200) {
+                        console.log(body)
+                    }
+                }
+            );
+        });
+
 };
+
